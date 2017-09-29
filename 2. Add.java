a412198479e2f1c9a235068162a19801b129cdb0
Aa-yush <31875557+Aa-yush@users.noreply.github.com>
2017-09-29 21:01:05 +0530

    Add_Two_Numbers

---
+++ 2. Add.java
@@ -0,0 +1,30 @@
+/*  Program  To Add Two Number 
+*
+*
+*
+*
+*
+*
+*   Author- Ayush Gupta
+*   Contact No- 8745056594
+*
+*/
+public class Add{
+	
+	// Now, we make an main method which is static and passing an array which is type of String
+	
+	public static void main(String[] args){
+		
+		int a=5 , b=3 , sum;   // Variable Initialization 
+		
+	 sum = a + b;
+	 
+	System.out.println( "ADDITION IS: "+sum ); // Print the result
+	
+	}
+	
+}	
+
+/* Output:- 
+	ADDITION IS: 8 */
+	
