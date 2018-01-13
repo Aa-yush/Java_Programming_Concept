c53094aa82857f0e92804d7f3ab4a663d4b2d23d
Ayush _Gupta <31875557+Aa-yush@users.noreply.github.com>
2018-01-13 22:53:27 +0530

    Variable Length argument (...) Three doted represent Array

---
+++ Varargs.java
@@ -0,0 +1,26 @@
+/* Program :- Variable Length argument (...) Three doted represent Array  
+*
+*
+*
+*
+*
+*
+*   Author- Ayush Gupta
+*   Contact No- 8745056594
+*
+*/
+
+class Varargs{
+	public static void main(String ... args){
+		System.out.println(args[1]);
+	}
+}
+
+/*
+Output:-
+C:\Users\ayush\Desktop\Uploaded GitHub>javac Varargs.java
+
+C:\Users\ayush\Desktop\Uploaded GitHub>java Varargs 12 11 43
+11
+
+*/
