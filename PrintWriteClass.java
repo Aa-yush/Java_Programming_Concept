1ee7c1ec43c828de7bf4173f4d8e972913a44f82
Ayush _Gupta <31875557+Aa-yush@users.noreply.github.com>
2018-01-13 22:51:44 +0530

    Print String through PrintWriter

---
+++ PrintWriteClass.java
@@ -0,0 +1,27 @@
+
+/* Program :-Print SString Through PrintWriter class    
+*
+*
+*
+*
+*
+*
+*   Author- Ayush Gupta
+*   Contact No- 8745056594
+*
+*/
+
+import java.io.*;
+
+public class PrintWriteClass{
+	public static void main(String[] args){
+	PrintWriter pw=new PrintWriter(System.out,true);
+	pw.println("My Name is Ayush");
+		
+	}
+}
+/*
+Output:
+My Name is Ayush
+
+*/
