a568956d6b9ed8abbfe353895a1e9e867ecea985
Ayush _Gupta <31875557+Aa-yush@users.noreply.github.com>
2018-01-13 22:50:59 +0530

    For - Each Loop

---
+++ ForEach.java
@@ -0,0 +1,36 @@
+/* Program :-Working of For-each Loop  
+*
+*
+*
+*
+*
+*
+*   Author- Ayush Gupta
+*   Contact No- 8745056594
+*
+*/
+
+public class ForEach{
+	public static void main(String [] args ){
+		int array[]={1,2,3,4,5,6,7,8,9,10};
+		
+		for(int x:array)
+			System.out.println("Array element is: "+x);
+		
+		
+	}
+}
+
+/*
+Output:-
+Array element is: 1
+Array element is: 2
+Array element is: 3
+Array element is: 4
+Array element is: 5
+Array element is: 6
+Array element is: 7
+Array element is: 8
+Array element is: 9
+Array element is: 10
+*/
